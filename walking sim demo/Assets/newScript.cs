f217a5ad2551a06fe558aea78ac4434a32c6223f
jinaroo <sohnjin@gmail.com>
2018-09-27 10:13:41 -0400

    new script changes

--- walking sim demo/Assets/newScript.cs
+++ walking sim demo/Assets/newScript.cs
@@ -6,7 +6,10 @@ public class newScript : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
+		//this is just a random comment
+		int i + 3;
+		i += 1;
+		Debug.log(i);
 	}
 	
 	// Update is called once per frame
